5189a9d91ad7336ea24e66f2cf6cdf30a1ab18fd
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2023-12-29 12:36:19 +0530

    added gradient effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-r min-h-screen grainy from-rose-800 to-teal-500">
+    <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="gradient absolute left-[50%]  "></div>
         <h1 className="font-semibold text-7xl text-center">
           AI <span className="text-green-600 font-bold">note taking</span>{" "}
           assistant.
